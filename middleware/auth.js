b7457caa897dc6dc60db349db26a6094504e7383
BETHANY FOLINO <bethany.folino@gmail.com>
2021-04-13 10:36:00 -0700

    Fixed x-auth-token

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ const config = require("config");
 // of middleware
 module.exports = function (req, res, next) {
   // Get the token from the header
-  const token = req.header(x - auth - token);
+  const token = req.header("x-auth-token");
 
   // Check if no token
   if (!token) {
