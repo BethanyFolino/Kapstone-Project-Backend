177b7be0b30ffa6ca8551ef206822f10cf2c0e49
BETHANY FOLINO <bethany.folino@gmail.com>
2021-04-16 14:49:47 -0700

    Put in get and delete routes for reviews with Jacob

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -37,4 +37,65 @@ router.post(
   }
 );
 
+// @route GET api/posts
+// @desc Get all posts
+// @access Private
+router.get("/", auth, async (req, res) => {
+  try {
+    const reviews = await Review.find().sort({ date: -1 });
+    res.json(reviews);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Errors");
+  }
+})
+
+// @route  GET api/posts/:id
+// @desc   Get post by ID
+// @access Private
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const review = await Review.findById(req.params.id);
+
+    if(!review) {
+      return res.status(404).json({ msg: "Review not found" });
+    }
+
+    res.json(review);
+  } catch (err) {
+    console.error(err.message);
+    if(err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Review not found" });
+    }
+    res.status(500).send("Server Errors");
+  }
+})
+
+// @route  DELETE api/posts
+// @desc   Delete a post
+// @access Private
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const review = await Review.findById(req.params.id);
+
+    if(!review) {
+      return res.status(404).json({ msg: "Review not found" });
+    }
+
+    if(review.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+    await review.remove();
+
+    res.json({ msg: "Review removed" });
+  } catch (err) {
+    console.error(err.message);
+    if(err.kind === "ObjectId") {
+      return res.status(404).json({ msg: "Review not found" });
+    }
+    res.status(500).send("Server Errors");
+  }
+})
+
 module.exports = router;
