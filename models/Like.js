6bbec84c4f22c4a47bef9fe8edc846fdc1df8080
Jacob Short <69212644+JacobShortKenzie@users.noreply.github.com>
2021-04-19 18:03:40 -0700

    changed like schema

--- models/Like.js
+++ models/Like.js
@@ -3,25 +3,18 @@ const Schema = mongoose.Schema;
 
 // Build Like Schema
 const LikeSchema = new Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
-  },
-  name: {
-    type: String,
-  },
-  imdbID: {
-    type: String,
+  imdbId: {
+    type: string,
     required: true,
   },
-  liked: {
-    type: Boolean,
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+  users: [
+    {
+      userid: {
+        type: Schema.Types.ObjectId,
+        ref: "users",
+      },
+    },
+  ],
 });
 
 module.exports = Like = mongoose.model("like", LikeSchema);
