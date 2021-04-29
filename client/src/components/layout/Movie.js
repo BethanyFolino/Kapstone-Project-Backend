2c721bcf26f270507b23b0f745e3b6b3cdb16362
Bethany Folino <bethanyfolino@Bethanys-MacBook-Pro.local>
2021-04-28 22:19:30 -0700

    fixed movie component so each result only appears once with its reviews

--- client/src/components/layout/Movie.js
+++ client/src/components/layout/Movie.js
@@ -16,18 +16,6 @@ const Movie = ({ Title, Year, Poster, imdbID }) => {
 
   return (
     <>
-      {/* <div className="movie">
-        <h1>{Title}</h1>
-        <h2>{Year}</h2>
-        <div>
-          <img src={Poster} alt={Title} />
-        </div>
-      </div>
-      <div>{likes}</div>
-      <button onClick={likeTheMovie}>Like</button>
-      {/* reviews - imdbID exists here */}
-      {/* <ReviewForm imdbID={imdbID} />
-      <Reviews imdbID={imdbID} />  */}
       <div className='movie-results'>
       <Card className="our-movies" style={{ width: "18rem" }}>
         <Card.Img variant="top" src={Poster} />
@@ -45,20 +33,6 @@ const Movie = ({ Title, Year, Poster, imdbID }) => {
           </Card.Text>
         </Card.Body>
       </Card>
-      {/* <ReviewForm imdbID={imdbID} />
-      <Reviews imdbID={imdbID} /> 
-      <Card style={{ width: '18rem' }}> */}
-  {/* <Card.Img variant="top" src={Poster} />
-  <Card.Body>
-    <Card.Title>Title: {Title}</Card.Title>
-    <Card.Text>
-     Year Created: {Year}
-    </Card.Text>
-    <Card.Text>
-    <Reviews imdbID={imdbID} />
-    </Card.Text>
-  </Card.Body> */}
-{/* </Card> */}
 </div>
     </>
   );
